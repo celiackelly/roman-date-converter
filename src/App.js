0ae0c865d57701ff78eb5dc8cc0c2b98734aa4e6
Celia Kelly <celiackelly@gmail.com>
2023-04-01 22:07:44 -0400

    Working month/day/year DateFieldset component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import DateFieldset from './components/DateFieldset';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Roman Date Calculator</h1>
       </header>
-    </div>
+      <form>
+          <DateFieldset />
+        </form>
+    </main>
   );
 }
 
---
+++ src/components/DateFieldset.js
@@ -0,0 +1,62 @@
+import { React, useState } from 'react'
+
+export default function DateFieldset() {
+  const today = new Date()  
+  const [month, setMonth] = useState(today.getMonth() + 1)
+  const [day, setDay] = useState(today.getDate())   
+  const [year, setYear] = useState(today.getFullYear())  
+
+  function calcDaysInMonth() {
+    let daysCount = 31
+    const isLeapYear = year % 4 === 0    //also need to add 100/400 rule 
+    if (month == 4 || month == 6 || month == 9 || month == 11) {
+        daysCount = 30
+    }
+    if (month == 2) {
+        daysCount = !isLeapYear ? 28 : 29
+    } 
+    return daysCount
+  }  
+  const dayOptions = Array.from(
+    {length: calcDaysInMonth()}, 
+    (_, i) => (
+        <option key={i + 1} value={i + 1}>{i + 1}</option>
+    )
+  )
+
+  return (
+    <fieldset>
+        <label><span className="sr-only">Month</span>
+            <select name="month" value={month} onChange={ (e)=> {
+                setMonth(Number(e.target.value));
+                }}>
+                <option value="1">January</option>
+                <option value="2">February</option>
+                <option value="3">March</option>
+                <option value="4">April</option>
+                <option value="5">May</option>
+                <option value="6">June</option>
+                <option value="7">July</option>
+                <option value="8">August</option>
+                <option value="9">September</option>
+                <option value="10">October</option>
+                <option value="11">November</option>
+                <option value="12">December</option>
+            </select>
+        </label>
+        <label><span className="sr-only">Day</span>
+            <select name="day" value={day} onChange={(e) => setDay(Number(e.target.value))}>
+                {dayOptions}
+            </select>
+        </label>
+        <label><span className="sr-only">Year</span>
+            <input name="year" type="number" min="1" value={year} 
+                onChange={ (e)=> {
+                setYear(Number(e.target.value));
+                }}>
+            </input>
+        </label>
+
+    </fieldset>
+  )
+}
