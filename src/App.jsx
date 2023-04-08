639fcc1c9897d31e4228652ed33925a323319987
Celia Kelly <celiackelly@gmail.com>
2023-04-08 15:08:25 -0400

    Add fasti images

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import "normalize.css";
-import "./App.css";
 import Header from "./components/Header";
 import Route from "./components/Route";
 import About from "./routes/About";
 import Home from "./routes/Home";
+
+import "normalize.css";
+import "./App.css";
+//make multiple image sizes - this is huge!
+import fasti from "./assets/fasti_praenestini.svg";
+import fastiPortrait from "./assets/fasti_praenestini_portrait.svg";
+import farmerCalendar from "./assets/farmer_calendar.svg";
+import fastiAmiternini from "./assets/fasti_amiternini.webp";
 
 function App() {
   return (
     <>
       <Header />
       <main>
+        {/* <img id="fasti" src={fastiPortrait}></img> */}
+        <img id="fasti" src={fasti}></img>
+        {/* <img id="fasti" src={farmerCalendar}></img> */}
+        {/* <img id="fasti" src={fastiAmiternini}></img> */}
         <Route path="/">
           <Home />
         </Route>
