a84bb6bd5413c49ec9e39a7926f358fec248a398
Celia Kelly <celiackelly@gmail.com>
2023-04-08 22:57:21 -0400

    Fix toggle button focus state

--- src/components/MobileMenuToggleButton.jsx
+++ src/components/MobileMenuToggleButton.jsx
@@ -5,18 +5,12 @@ export default function MobileMenuToggleButton({
   handleToggleMobile,
 }) {
   return (
-    <svg
-      className={className}
-      role="button"
-      onClick={handleToggleMobile}
-      tabIndex="0"
-      viewBox="0 0 100 80"
-      width="40"
-      height="40"
-    >
-      <rect width="100" height="10" rx="8"></rect>
-      <rect y="30" width="100" height="10" rx="8"></rect>
-      <rect y="60" width="100" height="10" rx="8"></rect>
-    </svg>
+    <button className={className} onClick={handleToggleMobile}>
+      <svg viewBox="0 0 100 80" width="40" height="40">
+        <rect width="100" height="8" rx="8"></rect>
+        <rect y="30" width="100" height="8" rx="8"></rect>
+        <rect y="60" width="100" height="8" rx="8"></rect>
+      </svg>
+    </button>
   );
 }
